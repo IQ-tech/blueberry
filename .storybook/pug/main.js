fc0bdc20e471ba64e150b5d50e0b043d267a74c5
Italo Fontes <italo5123@hotmail.com>
2020-10-15 15:53:19 -0300

    :art: separated dev scripts by stack

--- .storybook/pug/main.js
+++ .storybook/pug/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   addons: ["@storybook/addon-docs/preset"],
   stories: [
+    "../../src/stories/**/*.stories.mdx",
     "../../src/flavors/pug/**/*.stories.mdx",
     "../../src/flavors/pug/**/*.stories.js",
   ],
--- .storybook/react/main.js
+++ .storybook/react/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  "stories": [
+  stories: [
+    "../../src/stories/**/*.stories.mdx",
     "../../src/flavors/react/**/*.stories.mdx",
-    "../../src/flavors/react/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../src/flavors/react/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
