c779d2289e46ca608acfd6a0cbe6d2668b6450be
Italo Fontes <italo5123@hotmail.com>
2020-10-15 12:44:33 -0300

    :sparkles: base react storybook working

---
+++ .storybook/react/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../../src/flavors/react/**/*.stories.mdx",
+    "../../src/flavors/react/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
