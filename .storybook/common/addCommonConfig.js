9dd5aeb1943226141df91bfd20c8ca6d5a5d4e97
Italo Fontes <italo5123@hotmail.com>
2020-10-21 10:35:48 -0300

    :sparkles: added base styles to storybook playground

--- .storybook/common/addCommonConfig.js
+++ .storybook/common/addCommonConfig.js
@@ -2,6 +2,8 @@ const rupture = require("rupture");
 const postStylus = require("poststylus");
 const path = require("path");
 
+const baseStylesFile = path.resolve(__dirname, '../../src/core/base-styles/main.styl')
+
 module.exports = (config) => {
   config.resolve.modules.push(
     path.resolve(__dirname, "../../src"),
@@ -16,7 +18,11 @@ module.exports = (config) => {
       {
         loader: "stylus-loader",
         options: {
-          use: [postStylus(["rucksack-css", "autoprefixer"]), rupture()],
+					use: [postStylus(["rucksack-css", "autoprefixer"]), rupture()],
+					import: [
+						// load base styles into the playground
+						baseStylesFile
+					],
           preferPathResolver: "webpack",
         },
       },
