8a57daa047cf2520f581fb168dbfbd838d6a800b
Italo Fontes <italo5123@hotmail.com>
2021-01-14 10:32:23 -0300

    :sparkles: added tokens to main bundle

--- src/core/index.js
+++ src/core/index.js
@@ -6,3 +6,8 @@
 
 // styles entry-file
 import "./main.styl";
+
+// design tokens
+export { default as colors } from "./tokens/colors.json";
+export { default as layout } from "./tokens/layout.json";
+export { default as typography } from "./tokens/typography.json";
