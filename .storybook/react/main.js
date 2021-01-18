8a06863843de6b936be470789ad72182dbb86c6a
Italo Fontes <italo5123@hotmail.com>
2021-01-18 10:12:17 -0300

    :sparkles: added new font

--- .storybook/react/main.js
+++ .storybook/react/main.js
@@ -1,16 +1,16 @@
-const addCommonConfig = require("../common/addCommonConfig")
+const addCommonConfig = require("../common/addCommonConfig");
 
 module.exports = {
-  stories: [
-    "../../src/stories/**/*.stories.mdx",
-    "../../src/flavors/react/**/*.stories.mdx",
+	stories: [
+		"../../src/stories/**/*.stories.mdx",
+		"../../src/flavors/react/**/*.stories.mdx",
 		"../../src/flavors/react/**/*.stories.js",
-		"../../src/flavors/react/**/*.stories.tsx"
-  ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  webpackFinal: async (config, { configType }) => {
-    addCommonConfig(config);
+		"../../src/flavors/react/**/*.stories.tsx",
+	],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+	webpackFinal: async (config, { configType }) => {
+		addCommonConfig(config);
 
-    return config;
-  },
+		return config;
+	},
 };
