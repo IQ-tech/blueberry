f7245c07be991cb149f54a8dd0298c7c49da8c67
Italo Fontes <italo5123@hotmail.com>
2021-01-27 18:31:44 -0300

    :sparkles: mobile button working

--- src/flavors/react/components/legacy/HeaderClassic/hooks/index.tsx
+++ src/flavors/react/components/legacy/HeaderClassic/hooks/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function useClassicHeader({ isLogged }: { isLogged: boolean | undefined }) {
-  
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   function goToHomePage(e: React.MouseEvent<HTMLElement>) {
     e.preventDefault();
     // redirect to homepage (if unlogged) or dashboard (if logged)
   }
 
-  return { unloggedMenuItems: [], goToHomePage };
+  function toggleMobileMenu() {
+    //verify if is mobile
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  return {
+    unloggedMenuItems: [],
+    goToHomePage,
+    isMobileMenuOpen,
+    toggleMobileMenu,
+  };
 }
 
 export default useClassicHeader;
