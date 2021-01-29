28c42a34c8a1de3bed20cb06490370f7fc31e7dd
Italo Fontes <italo5123@hotmail.com>
2021-01-29 10:13:34 -0300

    :art: fixed menu structure

--- src/flavors/react/components/legacy/HeaderClassic/subcomponents/LoggedInNavigation.tsx
+++ src/flavors/react/components/legacy/HeaderClassic/subcomponents/LoggedInNavigation.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+const FistLetterIcon = () => {
+  
+}
+
+const DeskUsernameButton = () => {
+
+}
+
 const LoggedInNavigation = ({ username }) => {
   return (
     <div className="header-classic__logged-in-navigation">
-      <div className="header-classic__logged-in-desk-name">{username}</div>
+      <div className="header-classic__logged-in-desk-name-holder">{username}</div>
       <div className="header-classic__logged-menu">
         <p>cebola</p>
       </div>
