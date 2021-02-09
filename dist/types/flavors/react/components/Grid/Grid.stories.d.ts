2b0eb78885dcb39e86df8d1ecf2f170e489f1bb0
Italo Fontes <italo5123@hotmail.com>
2021-02-09 15:35:55 -0300

    :rocket: bundled lib

---
+++ dist/types/flavors/react/components/Grid/Grid.stories.d.ts
@@ -0,0 +1,8 @@
+import { Meta } from "@storybook/react/types-6-0";
+import "core/base-styles/main.styl";
+import "core/components/Grid.styl";
+declare const _default: Meta<import("@storybook/react/types-6-0").Args>;
+export default _default;
+export declare const Fixed: any;
+export declare const Fluid: any;
+//# sourceMappingURL=Grid.stories.d.ts.map
