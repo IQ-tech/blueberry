cacb06d0405b5555eff5f61b0e072f9126d85f14
Italo Fontes <italo5123@hotmail.com>
2021-02-09 09:39:12 -0300

    :sparkles: added grid pug implementation

---
+++ src/flavors/pug/components/Grid/Grid.stories.js
@@ -0,0 +1,14 @@
+import DemoOne from "./demos/DemoOne.pug";
+import DemoTwo from "./demos/DemoTwo.pug";
+import FluidGrid from "./demos/FluidGrid.pug";
+import "core/components/Grid.styl";
+
+export default {
+	title: "Componentes/layout/Grid",
+};
+
+export const Fixed = () => DemoOne({});
+
+export const Fluid = () => FluidGrid({});
+
+export const ColumnExample = () => DemoTwo({});
