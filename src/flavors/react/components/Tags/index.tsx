865849014876c8db48c05fef64563045afe63b36
victufell <victor.fellype.a@gmail.com>
2021-02-26 16:07:09 -0300

    feat: added tag component and css integration

---
+++ src/flavors/react/components/Tags/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface TagsProps extends React.HTMLAttributes<HTMLDivElement> {
+  /* Defining tag types */
+  type: 'bill' | 'provider',
+  label: string,
+
+  /* Possible status for provider */
+  providerStatus: 'connected' | 'payment_active' | 'connecting' | 'error' | 'pending',
+  /* Possible status for bill */
+  billStatus: 'available' | 'overdue' | 'accumulated' | 'charge' | 'paid'
+}
+
+const providerClassesConfig = {
+  'connected': '--connected',
+  'payment_active': '--payment-active',
+  'connecting': '--connecting',
+  'error': '--error',
+  'pending': '--pending-action'
+}
+
+const billClassesConfig = {
+  'available': '--available',
+  'overdue': '--overdue',
+  'accumulated': '--accumulated',
+  'charge': '--charge',
+  'paid': '--paid'
+}
+
+const Tags:React.FC<TagsProps> = ({ type, label, billStatus, providerStatus }) => {
+  const currentProviderClass = providerClassesConfig[providerStatus]
+  const currentBillClass = billClassesConfig[billStatus]
+  
+  const tagClasses = classNames('tag', {
+    '--bill': type === 'bill',
+    '--provider': type === 'provider',
+    [currentProviderClass]: providerStatus,
+    [currentBillClass]: billStatus
+  })
+
+  return (
+    <div className={tagClasses}>
+      <p className="tag__label">
+        {label}
+      </p>
+    </div>
+  )
+};
+
+export default Tags
