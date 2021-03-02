298a2582d381492d20a368913361a7a7a5804508
victufell <victor.fellype.a@gmail.com>
2021-03-02 12:01:04 -0300

    fix header map

--- src/flavors/react/components/Tabs/TabsHeader/index.tsx
+++ src/flavors/react/components/Tabs/TabsHeader/index.tsx
@@ -13,28 +13,31 @@ const TabsHeader = ({
 		'tabs-button--notification-active': isNotificationActive
 	})
 
-	const buttonsMap = data =>
-		data.map(({ title, notificationAmount }, index) => {
-			const buttonClasses = classnames('tabs-button__item', {
-				'tabs-button__item--active': index === activeIndex
-			})
-			return (
-				<li
-					key={index}
-					data-index={index}
-					className={buttonClasses}
-					onClick={onTabClick}
-					data-clicked={`btn-tab-${index}`}
-				>
-					<p className="tabs-button__item-text">
-						{title}
-						<span>{notificationAmount}</span>
-					</p>
-				</li>
-			)
-		})
-
-	return <ul className={listClass}>{buttonsMap(tabsHeader)}</ul>
+	return  (
+		<ul className={listClass}>
+			{
+				tabsHeader.map(({ title, notificationAmount }, index) => {
+					const buttonClasses = classnames('tabs-button__item', {
+						'tabs-button__item--active': index === activeIndex
+					})
+					return (
+						<li
+							key={index}
+							data-index={index}
+							className={buttonClasses}
+							onClick={onTabClick}
+							data-clicked={`btn-tab-${index}`}
+						>
+							<p className="tabs-button__item-text">
+								{title}
+								<span>{notificationAmount}</span>
+							</p>
+						</li>
+					)
+				})
+			}
+		</ul>
+	)
 }
 
 export default TabsHeader
