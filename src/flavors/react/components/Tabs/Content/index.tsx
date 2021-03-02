9ea2e26e67b4607808e86c96d80114c313e85141
victufell <victor.fellype.a@gmail.com>
2021-03-02 12:05:05 -0300

    fix children map

--- src/flavors/react/components/Tabs/Content/index.tsx
+++ src/flavors/react/components/Tabs/Content/index.tsx
@@ -2,24 +2,21 @@ import React from 'react'
 import classnames from 'classnames'
 
 const TabsContent = ({ activeIndex, children }) => {
-	const mapChildren = children => {
-		return (
-			<div className="tabs-content__list">
-				{children.map((child, index) => {
-					const showTabContent = index === activeIndex
-					const contentClass = classnames('tabs-content__list-holder', {
-						'tabs-content__list-holder--show-content': showTabContent
-					})
 
-					return (
-						<div className={contentClass} key={`tab-content-${index}`}>{child}</div>
-					)
-				})}
-			</div>
-		)
-	}
+	return (
+		<div className="tabs-content__list">
+			{children.map((child, index) => {
+				const showTabContent = index === activeIndex
+				const contentClass = classnames('tabs-content__list-holder', {
+					'tabs-content__list-holder--show-content': showTabContent
+				})
 
-	return Array.isArray(children) ? mapChildren(children) : children
+				return (
+					<div className={contentClass} key={`tab-content-${index}`}>{child}</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default TabsContent
