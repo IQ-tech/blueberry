186140d10b2a1a169ed5d532e848a47e3d3ea918
Italo Fontes <italo5123@hotmail.com>
2021-03-16 15:35:33 -0300

    :sparkles: added logo

---
+++ .storybook/common/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+
+export default create({
+  brandTitle: 'iq blueberry',
+  brandImage: 'https://static.iq360.com.br/images/general/blueberry-logo.png',
+	base: 'light'
+});
--- .storybook/pug/manager.js
+++ .storybook/pug/manager.js
@@ -2,6 +2,5 @@ import { addons } from '@storybook/addons';
 import theme from "../common/theme"
 
 addons.setConfig({
-  showPanel: true,
 	theme
 });
