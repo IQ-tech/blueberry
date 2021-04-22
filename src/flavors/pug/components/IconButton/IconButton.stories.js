98be8ff3015500bb734f7112bed4317784d88697
Italo Fontes <italo5123@hotmail.com>
2021-04-22 18:13:34 -0300

    :sparkles: added icon button

---
+++ src/flavors/pug/components/IconButton/IconButton.stories.js
@@ -0,0 +1,9 @@
+import IconButtonDemo from "./Demo.pug";
+import "core/components/Button.styl";
+
+export default {
+	title: "Componentes/IconButton",
+};
+
+
+export const Primary = () => IconButtonDemo();
