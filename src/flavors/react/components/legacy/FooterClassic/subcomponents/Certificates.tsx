d52f3f5ce7aedd339fc5fc1cd20d2bb7f24063ea
airton <airtonvancin@gmail.com>
2021-07-26 19:17:46 -0300

    fix: label title certificates

--- src/flavors/react/components/legacy/FooterClassic/subcomponents/Certificates.tsx
+++ src/flavors/react/components/legacy/FooterClassic/subcomponents/Certificates.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Certificates = () => (
   <div className="footer-classic__certificates">
-    <h2 className="footer-classic__certificates-title">Siga a gente</h2>
+    <h2 className="footer-classic__certificates-title">Certificados</h2>
     <div className="footer-classic__certificates-wrap">
 			<div className="footer-classic__certificates-image">
 				<img src="https://static.iq360.com.br/images/certificates/ssl.svg" alt="SSL Blindado" />
