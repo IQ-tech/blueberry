49ed22496b049de122e1a0bfefb272f1e8e9c061
airton <airtonvancin@gmail.com>
2021-08-02 15:01:56 -0300

    fix: images in react version

--- src/flavors/react/components/legacy/FooterClassic/subcomponents/Certificates.tsx
+++ src/flavors/react/components/legacy/FooterClassic/subcomponents/Certificates.tsx
@@ -5,13 +5,31 @@ const Certificates = () => (
     <h2 className="footer-classic__certificates-title">Certificados</h2>
     <div className="footer-classic__certificates-wrap">
 			<div className="footer-classic__certificates-image">
-				<img src="https://static.iq360.com.br/images/certificates/ssl.svg" alt="SSL Blindado" />
+				<img
+					src="https://static.iq360.com.br/images/certificates/ssl.svg" 
+					alt="SSL Blindado"
+					loading="lazy"
+					width="74px"
+					height="53px"
+				/>
 			</div>
 			<div className="footer-classic__certificates-image">
-				<img src="https://static.iq360.com.br/images/certificates/reclame-aqui.svg" alt="Ótimo Reclame Aqui!" />
+				<img
+					src="https://static.iq360.com.br/images/certificates/reclame-aqui.svg" 
+					alt="Ótimo Reclame Aqui!"
+					loading="lazy"
+					width="59px"
+					height="60px"
+				/>
 			</div>
 			<div className="footer-classic__certificates-image">
-				<img src="https://static.iq360.com.br/images/certificates/pci.svg" alt="PCI" />
+				<img
+					src="https://static.iq360.com.br/images/certificates/pci.svg" 
+					alt="PCI"
+					loading="lazy"
+					width="84px"
+					height="36px"
+				/>
 			</div>
     </div>
   </div>
