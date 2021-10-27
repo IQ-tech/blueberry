1152f24215ffff704ae7c2ff4bd7845e0e237514
Evellyn Lima <evellynlrl@hotmail.com>
2021-10-27 09:36:17 -0300

    Export CentralizedModal as a component

---
+++ dist/types/flavors/react/components/CentralizedModal/index.d.ts
@@ -0,0 +1,17 @@
+import React from "react";
+interface CentralizedModalProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+    title?: string;
+    text?: string;
+    dangerButtonText?: string;
+    buttonText?: string;
+    children?: any;
+    isActive: boolean;
+    hasOverlay: boolean;
+    hasCloseButton: boolean;
+    hasButtons: boolean;
+    handleDangerButton?: (...args: any) => any;
+    handleButton?: (...args: any) => any;
+    handleCloseModal?: (...args: any) => any;
+}
+declare const CentralizedModal: React.FC<CentralizedModalProps>;
+export default CentralizedModal;
