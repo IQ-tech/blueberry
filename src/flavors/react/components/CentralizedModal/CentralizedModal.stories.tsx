8b8f3a821251885674c9390ccab6ae378e4a6411
Evellyn Lima <evellynlrl@hotmail.com>
2021-11-04 11:27:01 -0300

    Fix CentralizedModal layout issues

--- src/flavors/react/components/CentralizedModal/CentralizedModal.stories.tsx
+++ src/flavors/react/components/CentralizedModal/CentralizedModal.stories.tsx
@@ -23,7 +23,6 @@ export default {
 const Template = (args) => <CentralizedModal {...args} />;
 
 export const CentralizedModalExample = Template.bind({});
-
 CentralizedModalExample.args = {
   title: "Tem certeza que deseja deletar as imagens?",
   text:
@@ -37,5 +36,21 @@ CentralizedModalExample.args = {
   hasOverlay: true,
   hasCloseButton: true,
   hasButtons: true,
+  useChildren: false,
   children: null,
+};
+
+const UsingChildrenTemplate = (args) => (
+  <CentralizedModal {...args}>
+    <h1>oi</h1>
+  </CentralizedModal>
+);
+
+export const CentralizedModalUsingChildren = UsingChildrenTemplate.bind({});
+CentralizedModalUsingChildren.args = {
+  isActive: true,
+  handleCloseModal: () => {},
+  hasOverlay: true,
+  hasCloseButton: true,
+  useChildren: true,
 };
