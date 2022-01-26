008cbe33b978b5565601164c4469b6cca2a6091d
Ruan Berté <berte.ruan@gmail.com>
2022-01-26 10:48:43 -0300

    feat: Add select field pug

---
+++ src/flavors/pug/components/form/SelectField/SelectField.stories.js
@@ -0,0 +1,9 @@
+import InputFieldDefault from "./Demos/Default.pug";
+
+import "core/components/Form.styl";
+
+export default {
+	title: "Componentes/form/SelectField",
+};
+
+export const Default = () => InputFieldDefault();
