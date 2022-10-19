c6fa81ea7fa3751c73597b6fc17f82dc2e9fb3eb
Italo Fontes <italo5123@hotmail.com>
2022-10-19 18:13:32 -0300

    fix: :sparkles: added foward ref to input element

--- src/flavors/react/components/form/InputField/index.tsx
+++ src/flavors/react/components/form/InputField/index.tsx
@@ -18,87 +18,91 @@ interface InputProps extends ModifiedInputProps, CommonFieldsProps {
   tooltipConfig?: TooltipProps;
   /** Define if it should render a numeric keyboard in mobile devices */
   useNumericKeyboard?: boolean;
-  inputRef?: React.MutableRefObject<any>
 }
 
-const InputField: React.FC<InputProps> = ({
-  htmlType = "text",
-  placeholder = "",
-  autoComplete = "off",
-  name,
-  value,
-  required,
-  errorMessage,
-  label,
-  optional,
-  invalid,
-  disabled,
-  Icon,
-  onChange,
-  customClass,
-  LeftIcon,
-  tooltipConfig,
-  useNumericKeyboard = false,
-  inputRef,
-  ...rest
-}) => {
-  const shouldRenderRightIcon = !invalid && !!Icon;
+const InputField: React.FC<InputProps> = React.forwardRef(
+  (
+    {
+      htmlType = "text",
+      placeholder = "",
+      autoComplete = "off",
+      name,
+      value,
+      required,
+      errorMessage,
+      label,
+      optional,
+      invalid,
+      disabled,
+      Icon,
+      onChange,
+      customClass,
+      LeftIcon,
+      tooltipConfig,
+      useNumericKeyboard = false,
+      ...rest
+    },
+    ref
+  ) => {
+    console.log(ref);
+    const shouldRenderRightIcon = !invalid && !!Icon;
 
-  function onChangeHandler(e) {
-    if (!!onChange) {
-      onChange(e?.target?.value);
+    function onChangeHandler(e) {
+      if (!!onChange) {
+        onChange(e?.target?.value);
+      }
     }
-  }
 
-  const inputClassName = classNames("iq-input-field", {
-    "iq-input-field--invalid": !!invalid,
-    "iq-input-field--disabled": !!disabled,
-    "iq-input-field--left-icon": !!LeftIcon,
-    [`iq-input-field--${customClass}`]: !!customClass,
-  });
+    const inputClassName = classNames("iq-input-field", {
+      "iq-input-field--invalid": !!invalid,
+      "iq-input-field--disabled": !!disabled,
+      "iq-input-field--left-icon": !!LeftIcon,
+      [`iq-input-field--${customClass}`]: !!customClass,
+    });
 
-  return (
-    <div className={inputClassName}>
-      <FieldBase
-        required={required}
-        errorMessage={errorMessage}
-        name={name}
-        label={label}
-        optional={optional}
-        invalid={invalid}
-        tooltipConfig={tooltipConfig}
-      >
-        <div className="iq-input-field__input-holder">
-          {!!LeftIcon ? (
-            <div className="iq-input-field__icon iq-input-field__icon--left">
-              <LeftIcon expand />
+    return (
+      <div className={inputClassName}>
+        <FieldBase
+          required={required}
+          errorMessage={errorMessage}
+          name={name}
+          label={label}
+          optional={optional}
+          invalid={invalid}
+          tooltipConfig={tooltipConfig}
+        >
+          <div className="iq-input-field__input-holder">
+            {!!LeftIcon ? (
+              <div className="iq-input-field__icon iq-input-field__icon--left">
+                <LeftIcon expand />
+              </div>
+            ) : null}
+
+            <input
+              disabled={disabled}
+              className="iq-input-field__input"
+              placeholder={placeholder}
+              type={htmlType}
+              onChange={onChangeHandler}
+              autoComplete={autoComplete}
+              value={value}
+              name={name}
+              inputMode={useNumericKeyboard ? "numeric" : undefined}
+              pattern={useNumericKeyboard ? "[0-9]*" : undefined}
+              ref={ref as any}
+              {...rest}
+            />
+            <div className="iq-input-field__icon iq-input-field__icon--right">
+              <div className="iq-input-field__invalid-icon">
+                <IconFilledError expand />
+              </div>
+              {!!shouldRenderRightIcon ? <Icon expand /> : null}
             </div>
-          ) : null}
-
-          <input
-            disabled={disabled}
-            className="iq-input-field__input"
-            placeholder={placeholder}
-            type={htmlType}
-            onChange={onChangeHandler}
-            autoComplete={autoComplete}
-            value={value}
-            name={name}
-            inputMode={useNumericKeyboard ? "numeric" : undefined}
-            pattern={useNumericKeyboard ? "[0-9]*" : undefined}
-            ref={inputRef}
-            {...rest}
-          />
-          <div className="iq-input-field__icon iq-input-field__icon--right">
-            <div className="iq-input-field__invalid-icon">
-              <IconFilledError expand />
-            </div>
-            {!!shouldRenderRightIcon ? <Icon expand /> : null}
           </div>
-        </div>
-      </FieldBase>
-    </div>
-  );
-};
+        </FieldBase>
+      </div>
+    );
+  }
+);
 
 export default InputField;
