3a1f40d801825c38fe925cd797a1589c83e48910
Tiago Vaz <tiago.vaz@hotmail.com>
2024-03-11 12:00:44 -0300

    feat: add callback functions on accordion

--- src/flavors/react/components/Accordion/types.ts
+++ src/flavors/react/components/Accordion/types.ts
@@ -1,35 +1,39 @@
-import * as React from "react";
+import * as React from 'react'
 
-type AccordionItemColorScheme = "purple-head" | "all-purple";
+type AccordionItemColorScheme = 'purple-head' | 'all-purple'
 
 export interface AccordionItemProps {
   /** if true, the Accoridon item starts opened */
-  open?: boolean;
+  open?: boolean
   /** index number inside the accordion (automatically assigned by the Accodion component)  */
-  accIndex?: number;
+  accIndex?: number
   /** Item children */
-  children: React.ReactNode;
+  children: React.ReactNode
   /** Item titleChildren */
-  titleChild?: React.ReactNode;
+  titleChild?: React.ReactNode
   /** List of all opened items */
-  openedItems?: number[];
+  openedItems?: number[]
   /** event to open Item */
-  onOpenItem?: (...args: any) => any;
+  onOpenItem?: (...args: any) => any
   /** event to close panel */
-  onCloseItem?: (...args: any) => any;
+  onCloseItem?: (...args: any) => any
   /** choose a pre-defined color scheme */
-  colorScheme?: AccordionItemColorScheme;
+  colorScheme?: AccordionItemColorScheme
+  /** run a function when item is opened */
+  openCallback?: (...args: any) => any
+  /** run a function when item is closed */
+  closeCallback?: (...args: any) => any
 }
 
 export interface AccordionType<T> extends React.FC<T> {
-  Item: React.FC<AccordionItemProps>;
+  Item: React.FC<AccordionItemProps>
 }
 
 export interface AccordionProps {
   /** Tells the accordion to keep only one item opened per time */
-  onlyOneItemOpen?: boolean;
+  onlyOneItemOpen?: boolean
   /** Accordion.Item[] */
-  children?: JSX.Element[];
+  children?: JSX.Element[]
   /** Set all accordion items a predefined color scheme */
-  colorScheme?: AccordionItemColorScheme;
+  colorScheme?: AccordionItemColorScheme
 }
