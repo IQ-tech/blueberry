6a9668e4bb3135d57ab87050ca42d1f2cd82f1a1
Erika Carvalho <scarvalho.erika@outlook.com>
2024-05-27 15:16:42 -0300

    chore: add new icons

---
+++ src/flavors/react/components/icons/generated/outline/OutlineCloseLight.tsx
@@ -0,0 +1,14 @@
+
+// This is a generated file, do not modify manually
+import * as React from "react"
+import Icon from "../../index";
+
+export default function OutlineCloseLight(props) {
+  return (
+    <Icon
+      iconMarkup={'<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.795 4.205a1.125 1.125 0 10-1.59 1.59L10.409 12l-6.205 6.205a1.125 1.125 0 101.591 1.59L12 13.592l6.204 6.205a1.125 1.125 0 101.591-1.591L13.591 12l6.204-6.204a1.125 1.125 0 00-1.59-1.591L12 10.409 5.795 4.205z" fill="#0048DB"/></svg>'}
+			iconName="OutlineCloseLight"
+      {...props}
+    />
+  );
+}
